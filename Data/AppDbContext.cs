f6f8974a6e5b01c3b479fdb7776f85c6f9920b99
aldiar1337 <vitugov2005@gmail.com>
2025-05-17 11:47:35 +0700

    3module

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Module3.Models;
+
+namespace Module3.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=module3;Username=postgres;Password=1234");
+    }
+}
+
+// dotnet ef migrations add "Test"
+//  dotnet ef database update       
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Module3.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string Login { get; set; }
+
+    public string Password { get; set; }
+
+    public bool IsFirstLogin { get; set; }
+
+    public bool IsAdmin { get; set; }
+
+    public bool IsBlocked { get; set; }
+
+    public int CounterFailed { get; set; }
+
+    public DateTime? LastLogin { get; set; }
+}
